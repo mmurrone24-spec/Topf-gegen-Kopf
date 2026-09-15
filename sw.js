5d4799159f016ee84f737a17e4020bff588889f9
MMaind <mmurrone24@gmail.com>
2026-09-15 14:49:31 +0000

    GameK&T

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'auto-explosion-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
